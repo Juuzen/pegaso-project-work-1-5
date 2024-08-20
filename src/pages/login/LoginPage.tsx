dcd51253fcd5d4cadc402d275b0a46288b8c2b3e
Michele Rega <mr.developer@outlook.it>
2024-08-20 17:32:22 +0200

    Added preliminary layout for Login Page

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,5 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { useAppStore } from "../../stores/AppStore";
+import { Button, Card, CardContent, Divider, TextField } from "@mui/material";
+import style from "./LoginPage.module.scss";
 
 const LoginPage = () => {
   const { login } = useAppStore();
@@ -11,9 +13,48 @@ const LoginPage = () => {
   };
 
   return (
-    <div style={{ width: 400, backgroundColor: "red" }}>
-      <h1>Login Page</h1>
-      <button onClick={handleLogin}>Login</button>
+    <div className={style.loginContainer}>
+      <Card>
+        <CardContent sx={{ padding: 0 }}>
+          <div className={style.cardContainer}>
+            <div className={style.cardTitleContainer}>
+              <div className={style.cardTitle}>PegasoFarm</div>
+              <div className={style.cardSubtitle}>
+                Sistema di gestione per aziende agricole
+              </div>
+            </div>
+
+            <Divider />
+
+            <div className={style.cardForm}>
+              <form>
+                <TextField
+                  variant="filled"
+                  id="login-username"
+                  label="Username"
+                  placeholder="admin"
+                  fullWidth
+                />
+                <TextField
+                  variant="filled"
+                  id="login-password"
+                  label="Password"
+                  type="password"
+                  fullWidth
+                />
+
+                <Button
+                  onClick={handleLogin}
+                  variant="contained"
+                  color="primary"
+                >
+                  LOGIN
+                </Button>
+              </form>
+            </div>
+          </div>
+        </CardContent>
+      </Card>
     </div>
   );
 };
